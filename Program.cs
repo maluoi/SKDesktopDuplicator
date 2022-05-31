d311b7447b3fb4556ea9a6106000970145d70ffb
Nick Klingensmith <programmerpichu@gmail.com>
2022-05-31 12:15:25 -0700

    - Adding some asset structure stuff

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using StereoKit.Framework;
 SK.AddStepper<DesktopDuplicator>();
 SK.AddStepper<PassthroughFBExt> ();
 
-if (!SK.Initialize(new SKSettings{ appName = "StereoKit Dream Dev" }))
+if (!SK.Initialize(new SKSettings{ appName = "StereoKit Dream Dev", assetsFolder = "Assets" }))
     return;
 
 SK.Run(()=>{
