428b534d056327c5fbc450c4bb3be0af83d82944
Nick Klingensmith <programmerpichu@gmail.com>
2022-07-29 16:43:08 -0700

    Added holographic remoting with command line settings & launch profiles to activate it!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿using StereoKit;
 using StereoKit.Framework;
+using StereoKit.HolographicRemoting;
+
+const string defaultIp = "192.168.1.231";
+int          remoteArg = Array.IndexOf(args, "-remote");
+if (remoteArg != -1)
+{
+	bool ipArg = (remoteArg + 1 < args.Length && args[remoteArg + 1].StartsWith("-") == false);
+	SK.AddStepper(new HolographicRemoting(ipArg ? args[remoteArg + 1] : defaultIp));
+}
 
 SK.AddStepper<DesktopDuplicator>();
 SK.AddStepper<PassthroughFBExt> ();
